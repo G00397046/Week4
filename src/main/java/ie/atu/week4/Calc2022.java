e8db41a0aa1ef2077a37e8d869aee59384f13417
alex Cronin <G00406014@atu.ie>
2022-10-11 14:23:08 +0100

    added delete feature

--- src/main/java/ie/atu/week4/Calc2022.java
+++ src/main/java/ie/atu/week4/Calc2022.java
@@ -1,7 +1,31 @@
 package ie.atu.week4;
 
-public class Calc2022 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Calc2022 {
+
+
+
+
+    public static void main (String[] args) {
+    delete();
     }
+
+    public static void delete() {
+    System.out.println("please enter your first number ");
+    Scanner inputs = new Scanner(System.in);
+
+    int firstnNumber = inputs.nextInt();
+
+
+
+    System.out.println("please enter your second number ");
+    int secondNumber = inputs.nextInt();
+
+
+        int total = firstnNumber - secondNumber;
+    System.out.println(" your total is :" + total );
+
+}
+
 }
