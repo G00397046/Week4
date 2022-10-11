e1b6b349646ea33f839fe6995ec776652ef823e6
G00397046 <114954044+G00397046@users.noreply.github.com>
2022-10-11 14:45:59 +0100

    Update Calc2022.java

--- src/main/java/ie/atu/week4/Calc2022.java
+++ src/main/java/ie/atu/week4/Calc2022.java
@@ -8,7 +8,22 @@ public class Calc2022 {
 
 
     public static void main (String[] args) {
+    add();
     delete();
+    }
+    
+     public static void add() {
+        System.out.println("Please enter first number: ");
+        Scanner inputs = new Scanner(System.in);
+
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter first number: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+
+        System.out.printf("The total is " + total);
     }
 
     public static void delete() {
