f8e6f092f62503eb7d7dcb8d9aaf6d6d0a5f1e15
Patrick Cahill <G00397046@atu.ie>
2022-10-11 14:08:49 +0100

    Added add feature to project

--- src/main/java/ie/atu/week4/Calc2022.java
+++ src/main/java/ie/atu/week4/Calc2022.java
@@ -1,7 +1,25 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc2022 {
     public static void main(String[] args) {
-
+        add();
     }
+
+    public static void add() {
+        System.out.println("Please enter first number: ");
+        Scanner inputs = new Scanner(System.in);
+
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter first number: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+
+        System.out.printf("The total is " + total);
+    }
+
 }
+
