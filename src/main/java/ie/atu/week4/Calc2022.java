4ddabb2b6fd9c7b52248cad2b938799308f207a7
Patrick Cahill <G00397046@atu.ie>
2022-10-11 15:06:53 +0100

    Fixed add branch

--- src/main/java/ie/atu/week4/Calc2022.java
+++ src/main/java/ie/atu/week4/Calc2022.java
@@ -3,24 +3,26 @@ package ie.atu.week4;
 import java.util.Scanner;
 
 public class Calc2022 {
-    public static void main(String[] args) {
-        add();
+
+        public static void main (String[] args) {
+           add();
+        }
+
+        public static void add() {
+            System.out.println("Please enter your first number to add: ");
+            Scanner inputs = new Scanner(System.in);
+
+            int firstNumber = inputs.nextInt();
+
+
+
+            System.out.println("Please enter your second number to add:  ");
+            int secondNumber = inputs.nextInt();
+
+
+            int total = firstNumber + secondNumber;
+            System.out.println(" your total is :" + total );
+
+        }
 
     }
-
-    public static void add() {
-        System.out.println("Please enter first number: ");
-        Scanner inputs = new Scanner(System.in);
-
-        int firstNumber = inputs.nextInt();
-
-        System.out.println("Please enter first number: ");
-        int secondNumber = inputs.nextInt();
-
-        int total = firstNumber + secondNumber;
-
-        System.out.printf("The total is " + total);
-    }
-
-}
-
