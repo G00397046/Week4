67ab923921deecc8384a6859a78f44b8cf4dd6ac
Patrick Cahill <G00397046@atu.ie>
2022-10-11 14:51:53 +0100

    Fixed error in delete

--- src/main/java/ie/atu/week4/Calc2022.java
+++ src/main/java/ie/atu/week4/Calc2022.java
@@ -1,7 +1,28 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc2022 {
-    public static void main(String[] args) {
+
+        public static void main (String[] args) {
+            delete();
+        }
+
+        public static void delete() {
+            System.out.println("please enter your first number ");
+            Scanner inputs = new Scanner(System.in);
+
+            int firstnNumber = inputs.nextInt();
+
+
+
+            System.out.println("please enter your second number ");
+            int secondNumber = inputs.nextInt();
+
+
+            int total = firstnNumber - secondNumber;
+            System.out.println(" your total is :" + total );
+
+        }
 
     }
-}
